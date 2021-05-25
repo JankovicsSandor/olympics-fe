1604d7ba0bfb218c8bf17e6ad0e11c80ca439050
JankovicsSandor <sanyika717022@gmail.com>
2021-05-25 20:06:16 +0200

    Change athlete model to interface

--- projects/models/src/lib/athlete/athlete.interface.ts
+++ projects/models/src/lib/athlete/athlete.interface.ts
@@ -1,16 +1,11 @@
 import { Sport } from "../sport";
 import { Gender } from "./gender.enum";
 
-export class Athlete {
+export interface Athlete {
   id: number;
   name: string;
-  get age() {
-    return this.currentTime.getFullYear() - this.dateOfBirth.getFullYear()
-  }
   dateOfBirth: Date;
   nationalityCode: string;
   gender: Gender
   sport: Sport
-
-  private currentTime = new Date();
 }
