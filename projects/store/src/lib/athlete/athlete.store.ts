e78a2653c18732342d3973c5ebe2756d4d06d50b
JankovicsSandor <sanyika717022@gmail.com>
2021-05-26 16:59:30 +0200

    Add default value

--- projects/store/src/lib/athlete/athlete.store.ts
+++ projects/store/src/lib/athlete/athlete.store.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import { EntityState, EntityStore, StoreConfig } from '@datorama/akita';
-import { Athlete } from '@models';
+import { Athlete, Gender, Sport } from '@models';
 
 export interface AthleteState extends EntityState<Athlete> { }
+
+const initialAtlete = [
+  <Athlete>{ dateOfBirth: new Date("2016.04.04"), name: "Josh Josh", height: 165.5, weight: 45, sport: Sport.Archery, nationalityCode: "GB", gender: Gender.Other }
+]
 
 @Injectable()
 @StoreConfig({ name: 'athlete' })
 export class AthleteStore extends EntityStore<AthleteState> {
 
   constructor() {
-    super();
+    super(initialAtlete);
   }
 
 }
